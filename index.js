8b367300ecc2f0a92893041bc903b6cf77933a26
SonawaneKuldeep8494 <99857060+SonawaneKuldeep29@users.noreply.github.com>
2024-03-22 23:30:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//This File is developed by kuldeep sonawane
 const csvtojsonv = require('csvtojson')
 
 const csvfilepath = "simple.csv"
-
+//This File is developed by kuldeep sonawane
 csvtojsonv()
 .fromFile(csvfilepath)
 .then((json)=>{
     console.log(json)
 })
+//This File is developed by kuldeep sonawane
